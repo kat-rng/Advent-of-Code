b6fd08c39a8734ff5dac32efccbcdcfac26de98d
kat <videomemes145@gmail.com>
2025-11-26 08:55:56 -0500

    Changing gitignore

--- 2024_01.hs
+++ 2024_01.hs
@@ -1,11 +1,21 @@
 import Data.List
-main :: IO ()
+import System.IO
 
 -- Take the sum of the absolute differences
 totalDiff x y = sum (map abs (zipWith (-) (sort x) (sort y)))
 
+readFixed 
+    
+
 -- Compare two lists
 main = do
-    let l1 = [3,4,2,1,3,3]
-    let l2 = [4,3,5,3,9,3]
-    putStrLn (show (totalDiff l1 l2))
+    --let l1 = [3,4,2,1,3,3]
+    --let l2 = [4,3,5,3,9,3]
+    --putStrLn (show (totalDiff l1 l2))
+
+    -- Test reading out the file
+    handle <- openFile "2024_01_input1" ReadMode
+    contents <- hGetContents handle
+    let x = lines contents
+    let y = map words x
+    putStr (head (head y))
