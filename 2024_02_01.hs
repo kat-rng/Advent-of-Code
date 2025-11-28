554bae907f19100c727727ed9fa7651cf7e11eea
kat <videomemes145@gmail.com>
2025-11-28 12:51:30 -0500

    Created code for part 1 day 2

--- 2024_02_01.hs
+++ 2024_02_01.hs
@@ -6,11 +6,48 @@ lineToInts :: String -> [Integer]
 lineToInts line =
     map read $ words line
 
+diffList :: [Integer] -> [Integer]
+diffList l = zipWith (-) (init l) (tail l)
+
+data ReactorDirection = Unknown
+        | Increasing
+        | Decreasing
+    deriving (Eq, Show)
+
+getState :: Integer -> ReactorDirection
+getState change
+    | change >  0 = Increasing
+    | change <  0 = Decreasing
+    | otherwise   = undefined
+
+checkState :: ReactorDirection -> [Integer] -> Bool
+checkState direction differences
+    | length differences == 0                           = True
+    | isUnsafeDiff $ head differences                   = False
+    | direction == Unknown || direction == currentDir   = checkState currentDir (tail differences)
+    | direction /= currentDir                           = False
+    | otherwise                                         = undefined
+    where currentDir = getState $ head differences
+
+newCheckState :: [Integer] -> Bool
+newCheckState = checkState Unknown
+
+isUnsafeDiff :: Integer -> Bool
+isUnsafeDiff difference
+    | difference >  3     = True
+    | difference < -3     = True
+    | difference == 0     = True
+    | otherwise           = False
+
 main = do
     -- Reading from the file
-    handle <- openFile "2024_01_input1" ReadMode
+    handle <- openFile "2024_02_input" ReadMode
     contents <- hGetContents handle
     let x = lines contents
 
     -- Find the integer list representation of each line
-    let matrix = map lineToInts x
+    let matrix      = map lineToInts x
+    let diffmatrix  = map diffList matrix
+    let safetys     = map newCheckState diffmatrix
+    let totalSafe   = sum $ map fromEnum safetys
+    putStr $ show totalSafe
