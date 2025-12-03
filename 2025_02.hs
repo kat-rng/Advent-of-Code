00fa274a71004e264ffe70b8475ed0e9b92e9edb
kat <videomemes145@gmail.com>
2025-12-02 22:07:17 -0500

    Created function to find repetition in integers

---
+++ 2025_02.hs
@@ -0,0 +1,19 @@
+import Math.NumberTheory.Logarithms
+
+isRepeatedDiv :: Integer -> Integer -> Bool
+isRepeatedDiv x slices
+    | size `mod` slices /= 0 = False
+    | slices /= 2 && mid == upper = isRepeatedDiv lower (slices-1)
+    | slices /= 2 = False
+    | upper == lower = True
+    | otherwise = False
+    where 
+        size   = fromIntegral (1 + integerLog10 x)
+        slicer = (10 ^ (size - size `div` slices))
+        slicerMid = (10 ^ (size - 2 * size `div` slices))
+        upper = x `div` slicer
+        lower = x `mod` slicer
+        mid   = lower `div` slicerMid
+
+
+
