7329b4cf23a38000ccde9ccab7165eb718de802a
katpy <92893261+kat-rng@users.noreply.github.com>
2025-12-04 12:00:51 -0500

    Creating a stencil to solve day 4 pt 1

--- app/F2025_04.hs
+++ app/F2025_04.hs
@@ -1,6 +1,7 @@
 module F2025_04 (pt1, pt2) where
 
-import Data.List
+import Prelude as P
+import Data.Massiv.Array as A
 import System.IO
 
 charToInt :: Char -> Int
@@ -9,17 +10,26 @@ charToInt c = case c of
     '@' -> 1
     _   -> error "Invalid character"
 
-badFunction :: Char -> Int
-badFunction a = a
+-- Converts a string representation of the input into a Massiv array
+inputToLists :: String -> Array U Ix2 Int
+inputToLists xs = A.fromLists' A.Seq intLists
+    where intLists = P.map (P.map charToInt) (lines xs)
 
--- Add this temporary function:
-checkLinter x = (head x)
+average3x3Stencil :: Integral a => Stencil Ix2 a a
+3x3Stencil = makeStencil (Sz (3 :. 3)) (1 :. 1) $ get ->
+        (  get (-1 :. -1) + get (-1 :. 0) + get (-1 :. 1) +
+           get ( 0 :. -1) +                 get ( 0 :. 1) +
+           get ( 1 :. -1) + get ( 1 :. 0) + get ( 1 :. 1)   )
+{-# INLINE average3x3Stencil #-}
 
+pt1 :: IO ()
 pt1 = do
     -- Reading from the file
     handle <- openFile "2025_04_input" ReadMode
     contents <- hGetContents handle
-    let lins = lines contents
+    let arr = inputToLists contents
+    let 
+    putStr $ show arr
 
 pt2 = do
     Nothing
