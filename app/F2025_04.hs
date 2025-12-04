0d9d4f7bb5d60647bb65ba43d5c9d27f90d51845
katpy <92893261+kat-rng@users.noreply.github.com>
2025-12-04 12:37:08 -0500

    Solving part 1 of advent of code with stencil operations in haskell

--- app/F2025_04.hs
+++ app/F2025_04.hs
@@ -2,7 +2,6 @@ module F2025_04 (pt1, pt2) where
 
 import Prelude as P
 import Data.Massiv.Array as A
-import Data.Massiv.Array.Stencil as S
 import System.IO
 
 charToInt :: Char -> Int
@@ -16,6 +15,7 @@ inputToLists :: String -> Array U Ix2 Int
 inputToLists xs = A.fromLists' A.Seq intLists
     where intLists = P.map (P.map charToInt) (lines xs)
 
+-- Create a stencil that finds the total neighbors that are paper rolls for each point
 sum3x3Stencil :: Integral a => Stencil Ix2 a a
 sum3x3Stencil = makeStencil (Sz (3 :. 3)) (1 :. 1) 
     (\get ->
@@ -26,14 +26,30 @@ sum3x3Stencil = makeStencil (Sz (3 :. 3)) (1 :. 1)
     )
 {-# INLINE sum3x3Stencil #-}
 
+-- Creating a padding to ensure the output from the stencil operation 
+-- will be the same dimensions as the input
+boxPad0 :: Padding Ix2 Int
+boxPad0 = Padding (Sz2 1 1) (Sz2 1 1) (Fill 0)
+
 pt1 :: IO ()
 pt1 = do
     -- Reading from the file
     handle <- openFile "2025_04_input" ReadMode
     contents <- hGetContents handle
+
+    -- Get the list contents
     let arr = inputToLists contents
-    let 
-    putStr $ show arr
+
+    -- Pad the array
+    let padArr = applyStencil boxPad0 sum3x3Stencil arr
+
+    -- Determine accessibility
+    let isAccessible = A.map (\x -> fromEnum (x<4)) (A.computeAs U padArr)
+    let isAccessibleU = A.computeAs U isAccessible
+
+    --Find the total accessible paper rolls
+    let totalAcc =  A.sum $ A.zipWith (*) isAccessibleU arr 
+    putStr $ show totalAcc
 
 pt2 = do
     Nothing
