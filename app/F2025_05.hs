4d9247b6032f6e06aa5b98dec4b6ffa66eea89e5
katpy <92893261+kat-rng@users.noreply.github.com>
2025-12-05 10:36:10 -0500

    Setup code to read the ranges and ids for day 5

---
+++ app/F2025_05.hs
@@ -0,0 +1,44 @@
+module F2025_05 (pt1, pt2) where
+
+import System.IO
+import Data.List.Split
+import Data.List
+
+-- Reads the ranges and ids 
+-- This assumes the blank line between them has been removed
+readInput :: String -> ([[Int]], [Int])
+readInput xs = do
+    -- Grab the lines and store them in a list of strings
+    let lins = lines xs
+
+    -- Grab the ranges by identifying that they have dashes in them
+    let ranges = map readRanges $ filter (\x -> '-' `elem`    x) lins
+
+    -- Do the reverse to find ids
+    let ids    = map read       $ filter (\x -> '-' `notElem` x) lins
+
+    -- Return both with the ranges transposed for easier lookup
+    (transpose ranges, ids)
+
+-- Read the two elements of the ranges by splitting on dashes
+readRanges :: String -> [Int]
+readRanges xs = map read $ splitOn "-" xs
+
+
+pt1 :: IO ()
+pt1 = do
+    -- Reading from the file
+    handle <- openFile "2025_05_input" ReadMode
+    contents <- hGetContents handle
+
+    -- Get the list contents
+    let (ranges, ids) = readInput contents
+
+    --Find the total accessible paper rolls with NO removals
+    putStr $ show ranges
+    putStr $ "\n\n" ++show ids
+
+pt2 :: IO ()
+pt2 = do
+    putStr "Nothing"
+
